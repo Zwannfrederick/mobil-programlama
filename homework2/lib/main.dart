af26230b3663c115f426b48dc4f0f5fd182a1f19
Zwann <memineminm120@gmail.com>
2024-10-15 18:37:25 +0300

    isimler ingilizce yapıldı, hatalar düzeltildi, uygulama derlendi

---
+++ homework2/lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'odev',
+      home: ColorTransitionScreen(),
+    );
+  }
+}
+
+class ColorTransitionScreen extends StatefulWidget {
+  @override
+  _ColorTransitionScreenState createState() => _ColorTransitionScreenState();
+}
+
+class ColorButton extends StatelessWidget {
+  final VoidCallback onPressed;
+  final Color backgroundColor;
+
+  ColorButton({required this.onPressed, required this.backgroundColor});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: backgroundColor,
+      ),
+      child: Text(
+        'Rengi Değiştir',
+        style: TextStyle(
+          color: backgroundColor == Color(0xFFFFD200)
+              ? Color(0xFF003DA5)
+              : Color(0xFFFFD200),
+        ),
+      ),
+    );
+  }
+}
+
+class _ColorTransitionScreenState extends State<ColorTransitionScreen> {
+  Color backgroundColor = Color(0xFF003DA5);
+  bool isNavyBlue = true;
+
+  void _toggleColor() {
+    setState(() {
+      backgroundColor = isNavyBlue ? Color(0xFFFFD200) : Color(0xFF003DA5);
+      isNavyBlue = !isNavyBlue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AnimatedContainer(
+        duration: Duration(milliseconds: 350),
+        color: backgroundColor,
+        child: Center(
+          child: ColorButton(
+            onPressed: _toggleColor,
+            backgroundColor: isNavyBlue ? Color(0xFFFFD200) : Color(0xFF003DA5),
+          ),
+        ),
+      ),
+    );
+  }
+}
